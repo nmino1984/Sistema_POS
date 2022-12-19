0fc01300626445d67c5c3297682ef63821352dc3
Noel Mino Herrera <o.kindelan@gmail.com>
2022-12-18 22:48:15 -0500

    Commit Until Video 3 done...

---
+++ POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using POS.Infrastructure.Persistences.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assembly = typeof(POSContext).Assembly.FullName;
+
+            services.AddDbContext<POSContext>(
+                options => options.UseSqlServer(
+                    configuration.GetConnectionString("POSConnectionString"), b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient);
+
+            return services;
+        }
+    }
+}
