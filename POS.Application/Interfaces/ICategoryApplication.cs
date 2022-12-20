3c1578693535561fc356f9b1b6214a8cdbd645f9
Noel Miño Herrera <noel.minoh@upr.edu.cu>
2022-12-20 14:23:05 -0500

    Started Video 9, but with Errors in CategortRequestViewModel and CategoryResponseViweModel

--- POS.Application/Interfaces/ICategoryApplication.cs
+++ POS.Application/Interfaces/ICategoryApplication.cs
@@ -1,4 +1,5 @@
 ﻿using POS.Application.Commons.Bases;
+using POS.Application.ViewModels.Request;
 using POS.Application.ViewModels.Response;
 using POS.Infrastructure.Commons.Bases.Request;
 using POS.Infrastructure.Commons.Bases.Response;
@@ -7,11 +8,11 @@ namespace POS.Application.Interfaces
 {
     public interface ICategoryApplication
     {
-        Task<BaseResponse<BaseEntityResponse<CategoryResponseViewModel>>> ListCategories(BaseFiltersRequest filters);
+        Task<BaseResponse<BaseEntityResponse<ViewModels.Response.CategoryResponseViewModel>>> ListCategories(BaseFiltersRequest filters);
         Task<BaseResponse<IEnumerable<CategorySelectResponseViewModel>>> ListSelectCategories();
-        Task<BaseResponse<CategorySelectResponseViewModel>> GetCategoriesById(int id);
-        Task<BaseResponse<bool>> RegisterCategory(CategoryResponseViewModel requestViewModel);
-        Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryResponseViewModel requestViewModel);
+        Task<BaseResponse<CategoryResponseViewModel>> GetCategoryById(int id);
+        Task<BaseResponse<bool>> RegisterCategory(CategoryRequestViewModel requestViewModel);
+        Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestViewModel requestViewModel);
         Task<BaseResponse<bool>> DeleteCategory(int id);
 
     }
--- POS.Application/Services/CategoryApplication.cs
+++ POS.Application/Services/CategoryApplication.cs
@@ -1,16 +1,15 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using POS.Application.Commons.Bases;
 using POS.Application.Interfaces;
 using POS.Application.Validators.Category;
+using POS.Application.ViewModels.Request;
 using POS.Application.ViewModels.Response;
+using POS.Domain.Entities;
 using POS.Infrastructure.Commons.Bases.Request;
 using POS.Infrastructure.Commons.Bases.Response;
 using POS.Infrastructure.Persistences.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using POS.Utilities.Statics;
 
 namespace POS.Application.Services
 {
@@ -28,32 +27,106 @@ namespace POS.Application.Services
         }
 
 
-        public Task<BaseResponse<BaseEntityResponse<CategoryResponseViewModel>>> ListCategories(BaseFiltersRequest filters)
+        public async Task<BaseResponse<BaseEntityResponse<CategoryResponseViewModel>>> ListCategories(BaseFiltersRequest filters)
+        {
+            var response = new BaseResponse<BaseEntityResponse<CategoryResponseViewModel>>();
+            var categories = await _unitOfWork.Category.ListCategories(filters);
+
+            if (categories is not null)
+            {
+                response.IsSuccess = true;
+                response.Data = _mapper.Map<BaseEntityResponse<CategoryResponseViewModel>>(categories);
+                response.Message = ReplyMessages.MESSAGE_QUERY;
+            }
+            else
+            {
+                response.IsSuccess = false;
+                response.Message = ReplyMessages.MESSAGE_QUERY_EMPTY;
+            }
+
+            return response;
+        }
+
+        public async Task<BaseResponse<IEnumerable<CategorySelectResponseViewModel>>> ListSelectCategories()
+        {
+            var response = new BaseResponse<IEnumerable<CategorySelectResponseViewModel>>();
+            var categories = await _unitOfWork.Category.ListSelectCategories();
+
+            if (categories is not null)
+            {
+                response.IsSuccess = true;
+                response.Data = _mapper.Map<IEnumerable<CategorySelectResponseViewModel>>(categories);
+                response.Message = ReplyMessages.MESSAGE_QUERY;
+            }
+            else
+            {
+                response.IsSuccess = false;
+                response.Message = ReplyMessages.MESSAGE_QUERY_EMPTY;
+            }
+
+            return response;
+
+        }
+
+        public async Task<BaseResponse<CategoryResponseViewModel>> GetCategoryById(int id)
+        {
+            var response = new BaseResponse<CategoryResponseViewModel>();
+            var category = await _unitOfWork.Category.GetBategoryById(id);
+
+            if (category is not null)
+            {
+                response.IsSuccess = true;
+                response.Data = _mapper.Map<CategoryResponseViewModel>(category);
+                response.Message = ReplyMessages.MESSAGE_QUERY;
+            }
+            else
+            {
+                response.IsSuccess = false;
+                response.Message = ReplyMessages.MESSAGE_QUERY_EMPTY;
+            }
+
+
+            return response;
+
+        }
+
+        public async Task<BaseResponse<bool>> RegisterCategory(CategoryRequestViewModel requestViewModel)
+        {
+            var response = new BaseResponse<bool>();
+            var validationResult = await _validationRules.ValidateAsync(requestViewModel);
+
+            if (!validationResult.IsValid) 
+            {
+                response.IsSuccess = false; 
+                response.Message = ReplyMessages.MESSAGE_VALIDATE;
+                response.Errors = validationResult.Errors;
+
+                return response;
+            }
+             
+            var category = _mapper.Map<Category>(requestViewModel);
+            response.Data = await _unitOfWork.Category.RegisterCategory(category);
+
+            if (response.Data)
+            {
+                response.IsSuccess = true;
+                response.Message = ReplyMessages.MESSAGE_SAVE;
+            }
+            else
+            {
+                response.IsSuccess = false;
+                response.Message = ReplyMessages.MESSAGE_FAILED;
+            }
+
+            return response;
+        }
+
+        public async Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestViewModel requestViewModel)
         {
             throw new NotImplementedException();
         }
 
-        public Task<BaseResponse<IEnumerable<CategorySelectResponseViewModel>>> ListSelectCategories()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<BaseResponse<CategorySelectResponseViewModel>> GetCategoriesById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<BaseResponse<bool>> RegisterCategory(CategoryResponseViewModel requestViewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryResponseViewModel requestViewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<BaseResponse<bool>> DeleteCategory(int id)
+        public async Task<BaseResponse<bool>> DeleteCategory(int id)
         {
             throw new NotImplementedException();
         }
